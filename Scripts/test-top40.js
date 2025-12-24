e83da2fc582d6278dd952aaa6a44c7fa92be9a4e
Sean Nicholas <sandblastchannel@gmail.com>
2025-12-23 21:55:40 -0500

    Add Top40Weekly regression test script

---
+++ Scripts/test-top40.js
@@ -0,0 +1,9 @@
+const kb = require("./Utils/musicKnowledge");
+
+const top = kb.getTopByYear(1984, 10, "Top40Weekly Top 100");
+
+top.forEach((m, i) => {
+  console.log(
+    (i + 1) + ". " + m.artist + ' - "' + m.title + '"'
+  );
+});
