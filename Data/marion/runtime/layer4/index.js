d945e7f7d5cc10adb4980da3a8d46e9574e76c2d
scnicholas <sandblastchannel@gmail.com>
2026-03-17 12:08:02 -0400

    index.js

--- Data/marion/runtime/layer4/index.js
+++ Data/marion/runtime/layer4/index.js
@@ -1,5 +1,3 @@
-// runtime/layer4/index.js
-
 const { resolveResponseMode } = require('./ResponseModeResolver');
 const { buildToneEnvelope } = require('./ToneEnvelopeBuilder');
 const { composeDomainResponse } = require('./DomainResponseComposer');
@@ -8,12 +6,46 @@ const { buildFallbackResponse } = require('./FallbackResponseBuilder');
 const { assembleResponse } = require('./ResponseAssembler');
 const { adaptForNyx } = require('./NyxOutputAdapter');
 
-async function runLayer4({ fusionPacket = {}, answerPlan = {} } = {}) {
-  const responseMode = resolveResponseMode({ fusionPacket, answerPlan });
-  const toneEnvelope = buildToneEnvelope({ fusionPacket, responseMode });
-  const domainResponse = composeDomainResponse({ fusionPacket, answerPlan, responseMode });
-  const safetyEnvelope = buildSafetyEnvelope({ fusionPacket, responseMode, toneEnvelope });
-  const fallbackResponse = buildFallbackResponse({ fusionPacket, responseMode });
+async function runLayer4({
+  fusionPacket = {},
+  answerPlan = {},
+  continuityState = {},
+  turnMemory = {}
+} = {}) {
+  const responseMode = resolveResponseMode({
+    fusionPacket,
+    answerPlan,
+    continuityState,
+    turnMemory
+  });
+
+  const toneEnvelope = buildToneEnvelope({
+    fusionPacket,
+    responseMode,
+    turnMemory
+  });
+
+  const domainResponse = composeDomainResponse({
+    fusionPacket,
+    answerPlan,
+    responseMode
+  });
+
+  const safetyEnvelope = buildSafetyEnvelope({
+    fusionPacket,
+    responseMode,
+    toneEnvelope,
+    turnMemory,
+    domainResponse
+  });
+
+  const fallbackResponse = buildFallbackResponse({
+    fusionPacket,
+    responseMode,
+    answerPlan,
+    continuityState,
+    turnMemory
+  });
 
   const assembled = assembleResponse({
     fusionPacket,
@@ -22,7 +54,9 @@ async function runLayer4({ fusionPacket = {}, answerPlan = {} } = {}) {
     toneEnvelope,
     domainResponse,
     safetyEnvelope,
-    fallbackResponse
+    fallbackResponse,
+    continuityState,
+    turnMemory
   });
 
   const nyxOutput = adaptForNyx(assembled);
