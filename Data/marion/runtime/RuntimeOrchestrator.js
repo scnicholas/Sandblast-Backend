b3ca6087b2d3fc3eab9bcc3c7353cae48eeb3e46
scnicholas <sandblastchannel@gmail.com>
2026-03-17 13:10:26 -0400

    RuntimeOrchestrator.js

--- Data/marion/runtime/RuntimeOrchestrator.js
+++ Data/marion/runtime/RuntimeOrchestrator.js
@@ -1,26 +1,55 @@
 // runtime/RuntimeOrchestrator.js
+"use strict";
 
-const { processWithMarion } = require('./MarionBridge');
+const { processWithMarion } = require("./marionBridge");
+
+function _safeObj(v) {
+  return v && typeof v === "object" && !Array.isArray(v) ? v : {};
+}
 
 async function runCognitivePipeline(input = {}) {
   const {
-    userQuery = '',
-    requestedDomain = '',
+    userQuery = "",
+    requestedDomain = "",
     conversationState = {},
-    datasets = []
+    datasets = [],
+    previousMemory = {},
+    domainEvidence = [],
+    datasetEvidence = [],
+    memoryEvidence = [],
+    generalEvidence = []
   } = input;
 
   const marionResult = await processWithMarion({
     userQuery,
     requestedDomain,
     conversationState,
-    datasets
+    datasets,
+    previousMemory,
+    domainEvidence,
+    datasetEvidence,
+    memoryEvidence,
+    generalEvidence
   });
 
   return {
-    ok: true,
-    pipeline: 'layer1-layer2-layer3',
-    result: marionResult
+    ok: !!marionResult.ok,
+    partial: !!marionResult.partial,
+    pipeline: "layer2-layer3-layer4-layer5",
+    result: marionResult,
+    diagnostics: {
+      domain: marionResult.domain || requestedDomain || "general",
+      intent: marionResult.intent || "general",
+      mode: _safeObj(marionResult.meta).mode || "balanced",
+      continuityHealth:
+        _safeObj(marionResult.turnMemory).continuityHealth ||
+        _safeObj(marionResult.continuityState).continuityHealth ||
+        "watch",
+      recoveryMode:
+        _safeObj(marionResult.turnMemory).recoveryMode ||
+        _safeObj(marionResult.continuityState).recoveryMode ||
+        "normal"
+    }
   };
 }
 
