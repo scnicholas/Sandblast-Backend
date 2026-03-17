97ff2e8ddcf1ed7688259f43985de97b847bdb02
scnicholas <sandblastchannel@gmail.com>
2026-03-17 01:37:38 -0400

    index.js

---
+++ Data/marion/runtime/layer5/index.js
@@ -0,0 +1,79 @@
+// runtime/layer5/index.js
+
+const { buildContinuityState } = require('./ContinuityStateBuilder');
+const { extractMemorySignals } = require('./MemorySignalExtractor');
+const { classifyPersistence } = require('./PersistenceClassifier');
+const { buildEmotionalContinuity } = require('./EmotionalContinuityEngine');
+const { buildDomainContinuity } = require('./DomainContinuityEngine');
+const { buildTopicThread } = require('./TopicThreadTracker');
+const { buildResetGuard } = require('./ResetGuard');
+const { assembleTurnMemory } = require('./TurnMemoryAssembler');
+
+async function runLayer5({
+  userQuery = '',
+  fusionPacket = {},
+  assembledResponse = {},
+  previousMemory = {}
+} = {}) {
+  const continuityState = buildContinuityState({
+    userQuery,
+    fusionPacket,
+    assembledResponse,
+    previousMemory
+  });
+
+  const extractedSignals = extractMemorySignals({
+    userQuery,
+    fusionPacket,
+    assembledResponse
+  });
+
+  const persistence = classifyPersistence(extractedSignals);
+
+  const emotionalContinuity = buildEmotionalContinuity({
+    fusionPacket,
+    previousMemory
+  });
+
+  const domainContinuity = buildDomainContinuity({
+    fusionPacket,
+    previousMemory
+  });
+
+  const topicThread = buildTopicThread({
+    userQuery,
+    previousMemory
+  });
+
+  const resetGuard = buildResetGuard({
+    continuityState,
+    emotionalContinuity,
+    domainContinuity,
+    topicThread
+  });
+
+  const turnMemory = assembleTurnMemory({
+    continuityState,
+    extractedSignals,
+    persistence,
+    emotionalContinuity,
+    domainContinuity,
+    topicThread,
+    resetGuard
+  });
+
+  return {
+    continuityState,
+    extractedSignals,
+    persistence,
+    emotionalContinuity,
+    domainContinuity,
+    topicThread,
+    resetGuard,
+    turnMemory
+  };
+}
+
+module.exports = {
+  runLayer5
+};
