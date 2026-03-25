f341121bb14b6b1048a4930a575a356a1a3770c7
scnicholas <sandblastchannel@gmail.com>
2026-03-25 00:23:27 -0400

    logger.js

--- src/jobs/newsCanada/logger.js
+++ src/jobs/newsCanada/logger.js
@@ -2,16 +2,44 @@ function stamp() {
   return new Date().toISOString();
 }
 
+function safeSerialize(value) {
+  if (value instanceof Error) {
+    return {
+      name: value.name,
+      message: value.message,
+      stack: value.stack
+    };
+  }
+
+  if (typeof value === "string") return value;
+
+  try {
+    return JSON.parse(JSON.stringify(value));
+  } catch (_) {
+    return String(value);
+  }
+}
+
+function normalizeArgs(args) {
+  return Array.from(args || []).map(safeSerialize);
+}
+
 function createLogger(prefix = "[log]") {
   function format(level, args) {
-    return [stamp(), prefix, level, ...args];
+    return [stamp(), prefix, level, ...normalizeArgs(args)];
+  }
+
+  function emit(method, level, args) {
+    const writer = typeof console[method] === "function" ? console[method] : console.log;
+    writer(...format(level, args));
   }
 
   return {
-    info: (...args) => console.log(...format("INFO", args)),
-    warn: (...args) => console.warn(...format("WARN", args)),
-    error: (...args) => console.error(...format("ERROR", args)),
-    debug: (...args) => console.debug(...format("DEBUG", args))
+    info: (...args) => emit("log", "INFO", args),
+    warn: (...args) => emit("warn", "WARN", args),
+    error: (...args) => emit("error", "ERROR", args),
+    debug: (...args) => emit("debug", "DEBUG", args),
+    child: (suffix = "") => createLogger(`${prefix}${suffix ? ` ${suffix}` : ""}`)
   };
 }
 
