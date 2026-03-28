0c8cb05686fd90b4c533438f311fbbc2f001f771
scnicholas <sandblastchannel@gmail.com>
2026-03-28 01:15:42 -0400

    config.js

--- src/jobs/newsCanada/config.js
+++ src/jobs/newsCanada/config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 
-const BASE_URL = "https://www.newscanada.com/home";
+const BASE_URL = "https://www.newscanada.com";
 const HOME_PATH = "/home";
 const HOME_URL = new URL(HOME_PATH, BASE_URL).toString();
+
+const DATA_DIR = path.resolve(__dirname, "data", "newscanada");
+const SNAPSHOT_DIR = path.resolve(DATA_DIR, "snapshots");
 
 const NEWS_CANADA_CONFIG = {
   baseUrl: BASE_URL,
@@ -19,8 +22,8 @@ const NEWS_CANADA_CONFIG = {
   maxEditorsPickLinks: 24,
   maxBodyNodesToScan: 1200,
   minBodyLength: 120,
-  outputDir: path.join(process.cwd(), "data", "newscanada"),
-  snapshotDir: path.join(process.cwd(), "data", "newscanada", "snapshots"),
+  outputDir: DATA_DIR,
+  snapshotDir: SNAPSHOT_DIR,
   outputFile: "editors-picks.v2.json",
   logPrefix: "[NewsCanadaV2]"
 };
