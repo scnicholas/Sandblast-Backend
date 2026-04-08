09db1c644cc2c884b186bd23f016a260a9687e68
scnicholas <sandblastchannel@gmail.com>
2026-04-07 22:10:33 -0400

    ttsProvidersResemble.js

--- Utils/ttsProvidersResemble.js
+++ Utils/ttsProvidersResemble.js
@@ -1,15 +1,24 @@
 "use strict";
 
-const tts = require("./tts");
+let tts = null;
+
+try { tts = require("./tts_consolidated"); } catch (_e) { tts = null; }
+if (!tts) {
+  try { tts = require("./tts"); } catch (_e) { tts = null; }
+}
 
 async function synthesize(opts) {
-  return tts.generate((opts && (opts.textSpeak || opts.text || opts.plainText || opts.textDisplay)) || "", opts || {});
+  if (!tts) throw new Error("No TTS runtime is available.");
+  if (typeof tts.generate === "function") {
+    return tts.generate((opts && (opts.textSpeak || opts.text || opts.plainText || opts.textDisplay)) || "", opts || {});
+  }
+  if (typeof tts.synthesize === "function") {
+    return tts.synthesize(opts || {});
+  }
+  throw new Error("TTS runtime does not expose generate() or synthesize().");
 }
 
 module.exports = {
   synthesize,
-  generate: tts.generate,
-  delegateTts: tts.delegateTts,
-  handleTts: tts.handleTts,
-  MANUAL_RESEMBLE_CONFIG: tts.MANUAL_RESEMBLE_CONFIG || Object.freeze({})
+  MANUAL_RESEMBLE_CONFIG: tts && tts.MANUAL_RESEMBLE_CONFIG ? tts.MANUAL_RESEMBLE_CONFIG : Object.freeze({})
 };
