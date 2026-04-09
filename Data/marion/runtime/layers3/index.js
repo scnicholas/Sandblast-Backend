06f6faddbdb275aecdcf27bad4693d01eb87bcfb
scnicholas <sandblastchannel@gmail.com>
2026-04-08 23:06:53 -0400

    index.js

--- Data/marion/runtime/layers3/index.js
+++ Data/marion/runtime/layers3/index.js
@@ -1,15 +1,25 @@
 // runtime/layer3/index.js
+"use strict";
 
-const { buildFusionPacket } = require('./FusionKernel');
-const { buildAnswerPlan } = require('./AnswerPlanBuilder');
+const { buildFusionPacket } = require("./FusionKernel");
+const { buildAnswerPlan } = require("./AnswerPlanBuilder");
+
+function _safeObj(v) { return v && typeof v === "object" && !Array.isArray(v) ? v : {}; }
 
 async function runLayer3(bundle = {}) {
   const fusionPacket = buildFusionPacket(bundle);
   const answerPlan = buildAnswerPlan(fusionPacket);
 
   return {
+    ok: true,
+    layer: "layer3",
     fusionPacket,
-    answerPlan
+    answerPlan,
+    diagnostics: {
+      ..._safeObj(fusionPacket.diagnostics),
+      answerPlanReady: true,
+      nyxDirectiveReady: !!_safeObj(answerPlan.nyxDirective).mode
+    }
   };
 }
 
