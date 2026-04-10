4dfeeb2259a209924053fba8f3265dda7af19b9a
scnicholas <sandblastchannel@gmail.com>
2026-04-10 00:02:16 -0400

    stateSpine.js

--- Utils/stateSpine.js
+++ Utils/stateSpine.js
@@ -14,7 +14,8 @@
  * - Stay fail-open safe when upstream signals are partial
  */
 
-const SPINE_VERSION = "stateSpine v1.4.0 SUPPORT-LOCK LOOP-HARDEN";
+const SPINE_VERSION = "stateSpine v1.5.0 SUPPORT-LOCK LOOP-HARDEN PRESENCE-SYNC";
+const STATE_SPINE_SCHEMA = "nyx.marion.stateSpine/1.5";
 const TERMINAL_AUDIO_STOP_MS = 30000;
 
 function safeStr(x) {
@@ -112,7 +113,9 @@ function createState(seed = {}) {
       continuityScore: 0.35,
       stateStreak: 0,
       placeholder: "Ask Nyx anything about Sandblast…",
-      lastActionLabels: []
+      lastActionLabels: [],
+      presenceState: "receptive",
+      listenerMode: "attuned"
     },
     lastUpdatedAt: 0
   };
@@ -531,6 +534,7 @@ function assertTurnUpdated(prevState, nextState) {
 }
 
 module.exports = {
+  STATE_SPINE_SCHEMA,
   SPINE_VERSION,
   TERMINAL_AUDIO_STOP_MS,
   createState,
