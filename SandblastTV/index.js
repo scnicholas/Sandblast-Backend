82999da4020e1e917032e65bed99bdd64050a2ec
scnicholas <sandblastchannel@gmail.com>
2026-07-17 16:57:50 -0400

    index.js

--- SandblastTV/index.js
+++ SandblastTV/index.js
@@ -5,27 +5,40 @@ const { createRouter } = require("./routes");
 const { ManifestStore } = require("./manifestStore");
 const { SchedulerService } = require("./schedulerService");
 
-function createSandblastTvRouter(options = {}) {
+function resolveDataDir(options = {}) {
   const rootDir = path.resolve(options.rootDir || path.join(__dirname, ".."));
-  const dataDir = path.resolve(
-    options.dataDir || process.env.SB_TV_DATA_DIR || path.join(rootDir, "Data", "SandblastTV")
+  return path.resolve(
+    options.dataDir ||
+    process.env.SB_TV_DATA_DIR ||
+    path.join(rootDir, "Data", "SandblastTV")
   );
-
-  const store = new ManifestStore({ dataDir });
-  const scheduler = new SchedulerService({ store });
-
-  return createRouter({ store, scheduler });
 }
 
 function createSandblastTvServices(options = {}) {
-  const rootDir = path.resolve(options.rootDir || path.join(__dirname, ".."));
-  const dataDir = path.resolve(options.dataDir || process.env.SB_TV_DATA_DIR || path.join(rootDir,"Data","SandblastTV"));
-  const store = new ManifestStore({dataDir});
-  const scheduler = new SchedulerService({store});
-  return {store,scheduler,router:createRouter({store,scheduler})};
+  const dataDir = resolveDataDir(options);
+  const store = new ManifestStore({
+    dataDir,
+    maxBackups: options.maxBackups || process.env.SB_TV_MAX_BACKUPS,
+    maxCertifications: options.maxCertifications || process.env.SB_TV_MAX_CERTIFICATIONS
+  });
+  const scheduler = new SchedulerService({ store });
+  const router = createRouter({
+    store,
+    scheduler,
+    adminToken: options.adminToken,
+    jsonLimit: options.jsonLimit,
+    certificationConcurrency: options.certificationConcurrency
+  });
+
+  return { dataDir, store, scheduler, router };
+}
+
+function createSandblastTvRouter(options = {}) {
+  return createSandblastTvServices(options).router;
 }
 
 module.exports = {
+  resolveDataDir,
   createSandblastTvRouter,
   createSandblastTvServices
 };
