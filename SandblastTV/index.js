31408dc595a027d5995bb11478da2dc6a15e3593
scnicholas <sandblastchannel@gmail.com>
2026-07-17 10:13:46 -0400

    index.js

--- SandblastTV/index.js
+++ SandblastTV/index.js
@@ -17,6 +17,15 @@ function createSandblastTvRouter(options = {}) {
   return createRouter({ store, scheduler });
 }
 
+function createSandblastTvServices(options = {}) {
+  const rootDir = path.resolve(options.rootDir || path.join(__dirname, ".."));
+  const dataDir = path.resolve(options.dataDir || process.env.SB_TV_DATA_DIR || path.join(rootDir,"Data","SandblastTV"));
+  const store = new ManifestStore({dataDir});
+  const scheduler = new SchedulerService({store});
+  return {store,scheduler,router:createRouter({store,scheduler})};
+}
+
 module.exports = {
-  createSandblastTvRouter
+  createSandblastTvRouter,
+  createSandblastTvServices
 };
