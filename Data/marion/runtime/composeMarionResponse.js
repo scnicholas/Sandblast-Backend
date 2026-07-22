84a6c9e79c60ac2961654df31cfcbdb8ba70aeda
scnicholas <sandblastchannel@gmail.com>
2026-07-22 18:43:13 -0400

    composeMarionResponse.js

--- Data/marion/runtime/composeMarionResponse.js
+++ Data/marion/runtime/composeMarionResponse.js
@@ -10000,3 +10000,26 @@ try{
 })();
 /* MARION_STRATEGIC_FLOW_LAYERS_15_16_17_COMPOSER_V17_END */
 
+/* MARION_COMPLETION_FLOW_LAYERS_18_19_20_COMPOSER_V20_START */
+(function marionCompletionFlowComposerV20(){
+  "use strict";
+  try{
+    const api=module.exports&&typeof module.exports==="object"?module.exports:null;if(!api)return;
+    const registry=require("./conversation/marionConversationLayerRegistry.js");
+    api.MARION_CONVERSATION_LAYERS_VERSION=registry.VERSION;
+    api.MARION_COMPLETION_FLOW_VERSION=registry.completionCoordinator&&registry.completionCoordinator.VERSION||"";
+    api.MARION_CROSS_DOMAIN_CONTEXT_VERSION=registry.crossDomainContextIntegrator&&registry.crossDomainContextIntegrator.VERSION||"";
+    api.MARION_GOAL_REALIGNMENT_VERSION=registry.goalRealignment&&registry.goalRealignment.VERSION||"";
+    api.MARION_DECISION_CLOSURE_VERSION=registry.decisionClosure&&registry.decisionClosure.VERSION||"";
+    api.getMarionCompletionFlowStatus=function(){return registry.getStatus();};
+    api.projectMarionCompletionFlowState=function(value){return registry.completionCoordinator.projectState(value&&value.completionFlow?value.completionFlow:value);};
+    api.marionConversationLayers=registry;
+    api.MARION_COMPLETION_FLOW_METADATA_PRIVATE=true;
+    api.MARION_LAYER_HARD_STOP=20;
+    api.MARION_ADDITIONAL_LAYER_RECOMMENDED=false;
+    api.MARION_COMPLETION_AUTOMATIC_EXECUTION_ALLOWED=false;
+    api.__marionCompletionFlowCapabilityV20=true;
+  }catch(_){}
+})();
+/* MARION_COMPLETION_FLOW_LAYERS_18_19_20_COMPOSER_V20_END */
+
